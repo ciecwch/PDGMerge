9e9d771e05e3c182dc90210c2fe7bd8a66e021e5
王春晖 <wangchunhui@wangchunhuideMacBook.local>
2017-06-13 12:19:38 +0800

    first

---
+++ src/PDGMerge.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by wangchunhui on 2017/4/10.
+ */
+public class PDGMerge {
+    public static void main(String[] args) throws Exception
+    {
+        String[] s={"4","100","100","testPdgMerge3.txt","test1.txt"};
+        Main m=new Main(s);
+    }
+}
